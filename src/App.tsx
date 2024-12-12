289006cd916d7a1efc5bfde8889f176f13259481
NikithaMedikonda <nikitha.medikonda@everest.engineering>
2024-12-12 13:44:14 +0530

    Add Initial router as Homepage

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import UserRegistrationPage from "./pages/UserRegistration";
 import UserLoginPage from "./pages/UserLogin";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<UserLoginPage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<UserLoginPage />} />
         <Route path="/register" element={<UserRegistrationPage />} />
       </Routes>
     </Router>
