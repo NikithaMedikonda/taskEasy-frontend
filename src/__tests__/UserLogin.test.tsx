3142b87e8d67423f17a8be422bcc9b055e2ce420
NikithaMedikonda <nikitha.medikonda@everest.engineering>
2024-12-12 13:11:15 +0530

    Remove test for navigating to register

--- src/__tests__/UserLogin.test.tsx
+++ src/__tests__/UserLogin.test.tsx
@@ -1,14 +1,7 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import UserLoginPage from '../pages/UserLogin';
-import { MemoryRouter } from 'react-router-dom';
 
 global.fetch = jest.fn();
-
-const mockNavigate = jest.fn();
-jest.mock("react-router-dom", () => ({
-    ...jest.requireActual("react-router-dom"),
-    useNavigate: () => mockNavigate,
-}));
 
 describe("LoginPage Component", () => {
     beforeAll(() => {
@@ -35,12 +28,5 @@ describe("LoginPage Component", () => {
 
         expect(screen.getByPlaceholderText(/Username/i)).toHaveValue("Nikitha");
         expect(screen.getByPlaceholderText(/Password/i)).toHaveValue("1234");
-    });
-
-    test("navigates to the registration page when clicking Register", () => {
-        render(<UserLoginPage/>)
-        fireEvent.click(screen.getByText(/Register/i));
-
-        expect(mockNavigate).toHaveBeenCalledWith("/register");
     });
 });
