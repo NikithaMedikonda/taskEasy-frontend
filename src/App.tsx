40ace3c8b4a4650639296156473ce398bb3100c2
NikithaMedikonda <nikitha.medikonda@everest.engineering>
2024-12-16 16:33:10 +0530

    Wrap app with context provider

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,20 @@ import UserRegistrationPage from "./pages/UserRegistration/UserRegistration";
 import UserLoginPage from "./pages/UserLogin/UserLogin";
 import HomePage from "./pages/HomePage/HomePage";
 import Dashboard from "./pages/DashBoard/Dashboard";
-import AddTaskForm from "./pages/AddTask/AddTask";
+import { UserProvider } from "./context/UserContext";
 
 function App() {
   return (
+    <UserProvider>
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<UserLoginPage />} />
           <Route path="/register" element={<UserRegistrationPage />} />
           <Route path="/dashboard" element={<Dashboard />} />
-          <Route
-            path="/addtask"
-            element={<AddTaskForm onSubmit={() => {}} />}
-          />
         </Routes>
       </Router>
+    </UserProvider>
   );
 }
 
